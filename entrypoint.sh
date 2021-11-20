c6beaa01874411355883a2ab9fde7f9e7044d983
KurokuTetsuya <perumnas.good@gmail.com>
2021-11-20 10:21:38 +0700

    fix(entrypoint): syntax error

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,11 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
+# Print Node.js Version
+node -v
+
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
